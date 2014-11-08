1cd689408ce7e4a25e2062d29fb7a63be5383341
Edward Aymerich <edward.aymerich@gmail.com>
2014-11-08 16:18:55 -0500

    Added probability calculations

--- Classifier.java
+++ Classifier.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 /**
  * This class is in charge of classifying emails as spam or not spam.
@@ -23,23 +24,55 @@ public class Classifier {
 	}
 	
 	private void calculateTokenProbabilities(){
-		// TODO: load training results, i.e., the file with 
-		//       words and their frequencies.
 		
-		// TODO: calculate token probabilities.
+		Scanner input = null;
+		try {
+			input = new Scanner(new File("filename"));
+		} catch (FileNotFoundException e) {
+			System.out.println("Could not open filename");
+		}
+		input.useDelimiter(" ");
+		
+		nbad = input.nextInt();
+		ngood = input.nextInt();
+		
+		// calculate token probabilities.
 		tokenProb = new HashMap<String,Double>;
+		for(int i = 0; 0 < tokens.size(); i++){
+			String token = input.next();
+			double b = input.nextInt();
+			double g = 2*(input.nextInt());
+			
+			double prob;
+			if(b+g < 5){
+				prob = defaultTokenProb;
+			}else{
+				prob = min(1.0,b/nbad) / ( min(1.0, g/ngood) + min(1.0, b/nbad) );
+				if (prob < 0.1) prob = 0.1;
+				if (prob > 0.99) prob = 0.99;
+			}
+			
+			tokenProb.add(token,prob);
+		}
 	}
 	
 	/**
 	 * Given a raw email string, this function classify such
 	 * email as spam (TRUE) or not spam (FALSE).
 	 */
-	public boolean classify(String email){
+	public int classify(String email){
 		// Use parser to counts words on email.
 		HashMap<String,Integer> tokenFreq = EmailParser.parseSingleEmail(email);
+		Scanner input = null;
+		try {
+			input = new Scanner(new File("filename"));
+		} catch (FileNotFoundException e) {
+			System.out.println("Could not open filename");
+		}
+		input.useDelimiter(" ");
 		
 		// Find most interesting tokens.
-		Champion champ = new Champion();
+		Champion champ = new Champion(15);
 		for (String token : tokenFreq.keySet()){
 			// Find probability for this token
 			double prob = 0.0;
@@ -53,16 +86,24 @@ public class Classifier {
 			champ.testContestant(token, prob);
 		}
 		
-		// Calculate combined probability
-		// TODO
+		// Calculate overall probability
 		double cprob = 0.5;
+		double prod = 1; 
+		double invProd = 1;
+		for(int i =0; i < champ.size(); i++){
+			prod *= champ.getChampionValue(i);
+			invProd *= (1-champ.getChampionValue(i));
+		}
+		double overallProb = prod / (prod + invProd);
 		
 		// Classify email as spam or not spam.
-		if (cprob > spamProbLimit){
-			return true;
+		if (overallProb > spamProbLimit){
+			return 1;
 		}
-		return false;
+		return 0;
 	}
+	
+	private int ngood, nbad;
 
 	/**
 	 * Probability assigned to a token not encountered during training.
