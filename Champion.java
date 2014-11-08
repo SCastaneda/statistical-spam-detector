d4c24969a80352ba1dd7c424b0c6e98dffabe061
Edward Aymerich <edward.aymerich@gmail.com>
2014-11-08 13:16:05 -0500

    Simplify Champion class.
    Class Champion has been simplify to store only one list of interesting
    tokens (or words).

--- Champion.java
+++ Champion.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 /**
  * This class keeps the words with highest and lowest probability of being spam.
- * For this, it keeps two sets of champions: high champions and low champions.
  * Each time a new "contestant" (a pair of word and value) appears it is 
  * compared with the current champions and gets stored if value is high or
  * low enough.
@@ -14,10 +13,9 @@ public class Champion {
 	 */
 	public Champion(){
 		n = 1;
-		lowVal = new ArrayList<Double>();
-		lowWord = new ArrayList<String>();
-		highVal = new ArrayList<Double>();
-		highWord = new ArrayList<String>();
+		cVals = new ArrayList<Double>();
+		vals = new ArrayList<Double>();
+		words = new ArrayList<String>();
 	}
 
 	/**
@@ -25,10 +23,9 @@ public class Champion {
 	 */
 	public Champion(int maxChampions){
 		n = maxChampions;
-		lowVal = new ArrayList<Double>();
-		lowWord = new ArrayList<String>();
-		highVal = new ArrayList<Double>();
-		highWord = new ArrayList<String>();
+		cVals = new ArrayList<Double>();
+		vals = new ArrayList<Double>();
+		words = new ArrayList<String>();
 	}
 	
 	/**
@@ -37,114 +34,82 @@ public class Champion {
 	 * among the appropriate champions (high or low).
 	 */
 	public void testContestant(String word, double value){
-		// Test for high value
-		if(highVal.isEmpty()){
+	
+		// Calculate competing value as distance from point of no interest.
+		double cValue = (value-pointOfNoInterest)*(value-pointOfNoInterest);
+	
+		// Test contestant.
+		if(cVals.isEmpty()){
 			// No competition. Just add the contestant.
-			highVal.add(value);
-			highWord.add(word);
+			cVals.add(cValue);
+			vals.add(value);
+			words.add(word);
 		}else{
 			// Test the contestant against the current champions.
 			int pos = 0;
-			while(pos < highVal.size() && value < highVal.get(pos)){
+			while(pos < cVals.size() && cValue < cVals.get(pos)){
 				pos++;
 			}
-			highVal.add(pos,value);
-			highWord.add(pos,word);
-			if(highVal.size() > n){
+			cVals.add(pos,cValue);
+			vals.add(pos,value);
+			words.add(pos,word);
+			if(cVals.size() > n){
 				// Full house. Somebody has to go. Let's kick the weakest one.
-				highVal.remove(n);
-				highWord.remove(n);
-			}
-		}
-		
-		// Test for low value
-		if(lowVal.isEmpty()){
-			// No competition. Just add the contestant.
-			lowVal.add(value);
-			lowWord.add(word);
-		}else{
-			// Test the contestant against the current champions.
-			int pos = 0;
-			while(pos < lowVal.size() && value > lowVal.get(pos)){
-				pos++;
-			}
-			lowVal.add(pos,value);
-			lowWord.add(pos,word);
-			if(lowVal.size() > n){
-				// Full house. Somebody has to go. Let's kick the weakest one.
-				lowVal.remove(n);
-				lowWord.remove(n);
+				cVals.remove(n);
+				vals.remove(n);
+				words.remove(n);
 			}
 		}
 	}
 	
-	public String getHighChampion(int pos){
-		if(pos < highWord.size()){
-			return highWord.get(pos);
+	public String getChampion(int pos){
+		if(pos < words.size()){
+			return words.get(pos);
 		}
 		return null;
 	}
 	
-	public double getHighChampionValue(int pos){
-		if(pos < highVal.size()){
-			return highVal.get(pos);
+	public double getChampionValue(int pos){
+		if(pos < vals.size()){
+			return vals.get(pos);
 		}
 		return -1.0;
 	}
 	
-	public String getLowChampion(int pos){
-		if(pos < lowWord.size()){
-			return lowWord.get(pos);
-		}
-		return null;
-	}
-	
-	public double getLowChampionValue(int pos){
-		if(pos < lowVal.size()){
-			return lowVal.get(pos);
-		}
-		return -1.0;
-	}
-	
-	public int highSize(){
-		return highVal.size();
-	}
-	
-	public int lowSize(){
-		return lowVal.size();
+	public int size(){
+		return cVals.size();
 	}
 
 	/**
 	 * Created just for testing class Champion.
 	 */
 	public static void main(String[] args) {
-		Champion champ = new Champion(3);
+		Champion champ = new Champion(5);
 		
-		champ.testContestant("three", 3);
-		champ.testContestant("one", 1);
-		champ.testContestant("two", 2);
-		champ.testContestant("six", 6);
-		champ.testContestant("four", 4);
-		champ.testContestant("five", 5);
-		champ.testContestant("seven", 7);
+		champ.testContestant("three", 0.3);
+		champ.testContestant("one", 0.1);
+		champ.testContestant("two", 0.2);
+		champ.testContestant("six", 0.6);
+		champ.testContestant("four", 0.4);
+		champ.testContestant("five", 0.5);
+		champ.testContestant("seven", 0.7);
 		
-		System.out.println("High Champions:");
-		for(int i = 0; i < champ.highSize(); i++){
-			System.out.println(champ.getHighChampion(i));
-		}
-		
-		System.out.println("Low Champions:");
-		for(int i = 0; i < champ.highSize(); i++){
-			System.out.println(champ.getLowChampion(i));
+		System.out.println("Champions:");
+		for(int i = 0; i < champ.size(); i++){
+			System.out.println(champ.getChampion(i));
 		}
 	}
 	
 	// Amount of champions in this container.
 	private int n;
 
+	// Value for point of no interest, i.e.,
+	// when the probability of a word being spam
+	// or not spam is the same.
+	private double pointOfNoInterest = 0.5;
+	
 	// Arrays that contains the current champion words.
-	ArrayList<Double> lowVal;
-	ArrayList<String> lowWord;
-	ArrayList<Double> highVal;
-	ArrayList<String> highWord;
+	ArrayList<Double> cVals; // competing values.
+	ArrayList<Double> vals; // real values.
+	ArrayList<String> words;
 }
