0bccd68ba1c338225815b8c21e76248b1840a3ff
Edward Aymerich <edward.aymerich@gmail.com>
2014-11-15 13:57:06 -0500

    Improved speed.
    Changed some Strings to StringBuffer, to improve speed.

--- CharFilter.java
+++ CharFilter.java
@@ -4,16 +4,18 @@ public class CharFilter implements Filter{
 	@Override
 	public String filter(String str) {
 		
-		String result = "";
+		StringBuffer result = new StringBuffer("");
 		
 		//String[] tokens = bodyStr.split("[^a-zA-Z\\d]");
 		//String[] tokens = bodyStr.split("\\s");
 		String[] tokens = str.split("[^\\p{Alnum}]");
 		for(String s : tokens){
-			if(s.length() > 0) result += (s + " ");
+			if(s.length() > 0){
+				result.append( s + " " );
+			}
 		}
 		
-		return result;
+		return result.toString();
 	}
 
 }
